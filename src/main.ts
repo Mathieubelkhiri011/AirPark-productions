5c49e8be583797b26b4989bf5c8f75f9e6519677
Mathieu BELKHIRI <mathieu.belkhiri@axopen.com>
2023-11-28 15:00:51 +0100

    feat: test deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
-import './assets/main.css'
-import './assets/styles/app.css'
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import router from '@/router'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(App)
 
 console.debug("Environment : " + import.meta.env.MODE);
 
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
