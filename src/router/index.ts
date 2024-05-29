34bb9329cda69c554b864f23fe83a00ed2bf079c
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-05-29 10:13:32 +0200

    deploy 6

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,30 +2,36 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home/Home.vue';
 import Navbar from '@/views/Navbar/Navbar.vue'
 
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    components: { default: Home, navbar: Navbar },
+  },
+  {
+    path: '/service/photo-video',
+    name: 'PhotoVideo',
+    components: { default: Home, navbar: Navbar },
+  },
+  {
+    path: '/service/inspection',
+    name: 'inspection',
+    components: { default: Home, navbar: Navbar },
+  },
+  {
+    path: '/service/personnalisable',
+    name: 'personnalisable',
+    components: { default: Home, header: Navbar },
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/home', // ou une route 404 personnalisée
+  },
+]
+
 const router = createRouter({
   history: createWebHistory('/AirPark-productions/'),
-  routes: [
-    {
-      path: '/home',
-      name: 'home',
-      components: { default: Home, navbar: Navbar },
-    },
-    {
-      path: '/service/photo-video',
-      name: 'PhotoVideo',
-      components: { default: Home, navbar: Navbar },
-    },
-    {
-      path: '/service/inspection',
-      name: 'inspection',
-      components: { default: Home, navbar: Navbar },
-    },
-    {
-      path: '/service/personnalisable',
-      name: 'personnalisable',
-      components: { default: Home, header: Navbar },
-    },
-  ]
+  routes,
 })
 
 export default router
