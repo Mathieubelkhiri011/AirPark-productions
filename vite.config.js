543d689cca77f48b42bd9fd0e665ad4af0ed0928
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-06-21 18:12:22 +0200

    correction bug pages #4

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-      Vue(),
-      Pages({
-        base: '/AirPark-productions/',
-      }),
-    ],
-    resolve: {
+  plugins: [vue()],
+  base: '/AirPark-productions/',
+  resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
