30eb7cff19291001f7f81c761c4aaacab8a3bb47
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-06-21 18:52:42 +0200

    correction bug pages #5

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
   base: '/AirPark-productions/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
-})
+});
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    devServer: {
-        headers: { "Cache-Control": "no-cache, no-store, must-revalidate" },
-        port: 8080,
-    },
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/AirPark-productions/'
-        : '/',
-    pwa: {
-        name: 'AirPark-productions',
-        themeColor: '#4e73df',
-    },
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/AirPark-productions/'
+    : '/',
+  devServer: {
+    headers: { "Cache-Control": "no-cache, no-store, must-revalidate" },
+    port: 8080,
+  },
+  pwa: {
+    name: 'AirPark-productions',
+    themeColor: '#4e73df',
+  },
 };
