fc28df483719e0102156825353e6dbee9373deff
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-06-21 18:01:53 +0200

    correction bug pages

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,12 @@ module.exports = {
         headers: { "Cache-Control": "no-cache, no-store, must-revalidate" },
         port: 8080,
     },
-    publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/AirPark-productions/'
+        : '/',
     pwa: {
         name: 'AirPark-productions',
         themeColor: '#4e73df',
     },
+
 };
