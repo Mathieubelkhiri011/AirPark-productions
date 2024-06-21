561f67dfc8accff7758ce9b02d2afff04ab8e04a
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-06-21 19:03:58 +0200

    correction bug pages #8

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home/Home.vue';
 import Contact from '@/views/Contact/Contact.vue';
 import Navbar from '@/views/Navbar/Navbar.vue';
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/AirPark-productions/'),
+  history: createWebHashHistory(),
   routes,
 })
 
