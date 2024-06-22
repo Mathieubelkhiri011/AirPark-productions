e4392369a42710b45665110c07f4e06921965fa2
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-06-22 19:53:45 +0200

    correction bug pages #15

--- src/components/shared/recaptcha/Recaptcha.ts
+++ src/components/shared/recaptcha/Recaptcha.ts
@@ -10,11 +10,24 @@ export default {
     onCaptchaVerified(response) {
       this.$emit("verify", response);
     },
+    renderCaptcha() {
+      if (window.grecaptcha) {
+        window.grecaptcha.render(this.$el.querySelector('.g-recaptcha'), {
+          sitekey: this.siteKey,
+          callback: this.onCaptchaVerified,
+        });
+      } else {
+        console.error('reCAPTCHA library not loaded');
+      }
+    }
   },
   mounted() {
-    window.grecaptcha.render(this.$el.querySelector('.g-recaptcha'), {
-      sitekey: this.siteKey,
-      callback: this.onCaptchaVerified,
-    });
+    const intervalId = setInterval(() => {
+      if (window.grecaptcha) {
+        clearInterval(intervalId);
+        this.renderCaptcha();
+      }
+    }, 100);
   },
+  template: `<div class="g-recaptcha"></div>`,
 };
