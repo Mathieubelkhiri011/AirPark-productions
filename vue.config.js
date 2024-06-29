d826ce260d9cdb8094cbdbf4ac43282160ca3b9c
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-06-29 22:34:55 +0200

    reset deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/AirPark-productions/'
+    : '/',
   devServer: {
     headers: { "Cache-Control": "no-cache, no-store, must-revalidate" },
     port: 8080,
