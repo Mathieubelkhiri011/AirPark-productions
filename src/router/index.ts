df17fc3dda2be5af3a22b7a77ee6df8b2ce688bd
Mathew <mathieu.belkhiri@etu.univ-lyon1.fr>
2024-07-04 16:20:49 +0200

    deployment #3

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ const routes = [
   },
   {
     path: '/:catchAll(.*)',
-    components: { default: Home, navbar: Navbar },
+    redirect: '/home',
   },
 ];
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: '/',
   devServer: {
     headers: { "Cache-Control": "no-cache, no-store, must-revalidate" },
     port: 8080,
